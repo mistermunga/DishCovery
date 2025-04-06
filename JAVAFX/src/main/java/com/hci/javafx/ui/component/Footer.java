97de3d172f044cdf9a8a24d8983b74dd102c4512
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-04-06 20:29:53 +0300

    added the Footer

---
+++ JAVAFX/src/main/java/com/hci/javafx/ui/component/Footer.java
@@ -0,0 +1,55 @@
+package com.hci.javafx.ui.component;
+
+import java.awt.Desktop;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.layout.HBox;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Objects;
+
+public class Footer extends HBox {
+
+    public Footer() {
+
+        this.getStylesheets()
+                .add(Objects.requireNonNull(getClass()
+                        .getResource("/com/hci/javafx/styles.css")).toExternalForm());
+
+        Hyperlink aboutUs = new Hyperlink("About Us");
+        aboutUs.getStyleClass().add("footer-link");
+        aboutUs.setOnAction(event -> sendToGithub());
+
+        Hyperlink feedback = new Hyperlink("Feedback");
+        feedback.getStyleClass().add("footer-link");
+        feedback.setOnAction(event -> sendToSurvey());
+
+        Hyperlink policy = new Hyperlink("Privacy Policy");
+        policy.getStyleClass().add("footer-link");
+        policy.setOnAction(event -> {System.out.println("Coming soon...");});
+
+        this.getChildren().addAll(aboutUs, feedback, policy);
+
+        this.setAlignment(Pos.CENTER);
+        this.setSpacing(40);
+    }
+
+    private void sendToSurvey() {openWebPage("https://docs.google.com/forms/d/e/1FAIpQLSdMn1UZINVFN10Fz6msCNT_LOlGO1lBHaDwQL1vLYTvp-NjKw/viewform?usp=dialog");}
+
+    private void sendToGithub() {openWebPage("https://github.com/mislisdis/DishCovery");}
+
+    private void openWebPage(String url) {
+        try {
+            if (Desktop.isDesktopSupported()) {
+                Desktop.getDesktop().browse(new URI(url));
+            } else {
+                System.err.println("Desktop is not supported");
+            }
+        } catch (IOException | URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
